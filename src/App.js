c6db19aef9a5def827b2100718d561889d09f9e9
discountry <331027793@qq.com>
2018-11-14 02:17:31 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Chart from './Chart';
+import { getData } from './binance';
+import { getParams } from './utils';
+
+import { TypeChooser } from "react-stockcharts/lib/helper";
+import domtoimage from 'dom-to-image';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+  componentDidMount() {
+    let symbol = 'BTCUSDT'
+    let interval = '1d'
+    let limit = 500
+    if (getParams(window.location.search)) {
+      [symbol, interval, limit] = getParams(window.location.search)
+    }
+		getData(symbol, interval, limit).then(data => {
+      this.setState({ data })
+		}).then(() => {
+      var node = document.getElementById('root');
+      domtoimage.toPng(node)
+          .then(function (dataUrl) {
+              var img = new Image();
+              img.src = dataUrl;
+              document.body.innerHTML = `<img src="${dataUrl}">`;
+          })
+          .catch(function (error) {
+              console.error('oops, something went wrong!', error);
+          });
+    })
+	}
+	render() {
+		if (this.state == null) {
+			return <div>Loading...</div>
+		}
+		return (
+			<TypeChooser>
+				{type => <Chart type={type} data={this.state.data} />}
+			</TypeChooser>
+		)
+	}
 }
 
 export default App;
