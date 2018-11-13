1f1485904ff5db6a83af467a6c93362cc3485a83
discountry <331027793@qq.com>
2018-11-14 02:34:34 +0800

    charter

---
+++ precache-manifest.3d1fbc5bdf28270f0509a98393ce97dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7dbbc46c4e7595b3ea5b",
+    "url": "/static/js/main.7dbbc46c.chunk.js"
+  },
+  {
+    "revision": "ed2adce80f8f0de1cf8a",
+    "url": "/static/js/1.ed2adce8.chunk.js"
+  },
+  {
+    "revision": "7dbbc46c4e7595b3ea5b",
+    "url": "/static/css/main.a7f00a55.chunk.css"
+  },
+  {
+    "revision": "a15772b63cec86598f34403fe45249fd",
+    "url": "/index.html"
+  }
+];
--- src/utils.js
+++
@@ -1,4 +0,0 @@
-export function getParams(url) {
-	var urlParams = new URLSearchParams(url);
-	return [urlParams.get('symbol') ? urlParams.get('symbol') : 'BTCUSDT', urlParams.get('interval') ? urlParams.get('interval') : '1d', urlParams.get('limit') ? urlParams.get('limit') : 500]
-}
